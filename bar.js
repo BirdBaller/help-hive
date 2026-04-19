0a96919b4daf497771247fd17ec255ad236d0e06
BirdBaller <poisonous.wolf34@gmail.com>
2026-04-19 13:31:25 -0400

    Delete bar.js

--- bar.js
+++
@@ -1,3 +0,0 @@
-  const switched = document.getElementById("LightSwitch");
-  switched.checked = localStorage.switched === "1";
-  switched.onchange = () => localStorage.switched = switched.checked ? "1" : "0";
